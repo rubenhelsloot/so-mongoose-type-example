15574426ad822c4c34b19078eb4b5ac01d136899
Ruben Helsloot <rubenhelsloot@pieter-pot.nl>
2023-01-17 12:09:46 +0100

    add file with example usage

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { Customer } from "./models";
+
+function useStatic() {
+  return Customer.findByEmail('test@example.com');
+}
+
+async function useMethod() {
+  const customer = await Customer.findOne().exec();
+  customer // has type `any`
+  customer.markDeleted();
+}
+
+async function accessVirtualProperty() {
+  const customer = await Customer.findOne().exec();
+  customer // has type `any`
+  return customer.fullName;
+}
